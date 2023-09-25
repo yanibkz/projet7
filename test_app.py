ff4a28385e145bf6b0a5b0a87c23bd5714739978
Boukhezar <boukhezar.yani@hotmail.com>
2023-09-25 02:11:55 +0200

    upt test

--- test_app.py
+++ test_app.py
@@ -24,18 +24,3 @@ def test_features(client):
     response = client.get('/features')
     assert response.status_code == 200
     # Vérifiez le contenu de la réponse ici
-
-def test_features_plot(client):
-    response = client.get('/features_plot')
-    assert response.status_code == 200
-    assert response.mimetype == 'image/png'
-
-def test_shap_plot(client):
-    response = client.get(f'/shap_plot/{random_id}')  # Utilisez un ID qui est effectivement dans initial_df
-    assert response.status_code == 200
-    assert response.mimetype == 'image/png'
-
-def test_distribution_plots(client):
-    response = client.get('/distribution-plots/1/SK_ID_CURR')  # Utilisez un ID et une colonne qui sont effectivement dans initial_df
-    assert response.status_code == 200
-    assert response.mimetype == 'image/png'
